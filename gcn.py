9cbb556a50de5505fc59e0bdbfa75ebba569c23e
Sandipan Sikdar <sandipansikdar@178-146.eduroam.rwth-aachen.de>
2018-07-18 17:00:33 +0200

    gcn_complete

--- gcn.py
+++ gcn.py
@@ -25,6 +25,15 @@ def readFeatureMatrix(fname):
     feat_vec = np.loadtxt(fname)
     return torch.tensor(feat_vec,dtype=torch.float64)
 
+def readLabels(fname,nodes):
+    labels = torch.zeros(nodes)
+    i = 0
+    with open(fname) as fs:
+        ind = int(line.strip())
+        labels[i] = ind
+        i+=1
+    return labels
+
 class GCN(nn.Module):
     def __init__(self,input_size,hidden_size,output_size):
         super(GCN, self).__init__()
@@ -48,14 +57,33 @@ class GCN(nn.Module):
         return output
 
 
-def train(gcn,norm_adj,feat_vec,iterations=1000,lr=0.001):
+def train(gcn,train_norm_adj,train_feat_vec,train_labels,iterations=1000,lr=0.001):
+    optimizer = optim.SGD(vector.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+
+    output = gcn(train_norm_adj, train_feat_vec)
+
+    loss = criterion(output,train_labels)
+
+    optimizer.zero_grad()
+    loss.backward(retain_graph=True)
+    optimizer.step()
+
+def evaluate(gcn,test_norm_adj,test_feat_vec,test_labels):
+
+    output = gcn(test_norm_adj, test_feat_vec)
+    value,index = torch.topk(output,1)
+    for i in range(index.size[0]):
+        print('✓' if index[i]==label[i] else '✗')
 
 
-G = readGraph('')
+G = readGraph('input_network_file')
 norm_adj = torch.tensor(normalizedLaplacian(G),dtype=torch.float64)
-feat_vec = readFeatureMatrix('')
+feat_vec = readFeatureMatrix('input_feature_file')
+labels = readLabels('labels_file')
+train_norm_adj,train_feat_vec,train_labels,test_norm_adj,test_feat_vec,test_labels = obtainTestTrainSplit(norm_adj,feat_vec,labels) # implement based on input data...
 hidden_size = 20
 input_size = feat_vec.shape[1]
-output_size = 30
+output_size = 10 # number of output classes
 gcn = GCN(input_size, hidden_size, output_size)
 train(gcn,norm_adj,feat_vec)
